dba1ae6730ad697f83cc8b671588670c38de735a
Ezequiel Colunga Acha <ezequielcolungaacha@gmail.com>
2026-01-12 18:53:57 -0300

    pkg and vite cfg

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ssb/',
+  base: process.env.LOCAL_DEPLOY ? '/' : '/ssb/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
